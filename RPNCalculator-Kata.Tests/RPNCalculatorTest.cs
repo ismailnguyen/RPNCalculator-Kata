427345247379fdf5d948c0fc2af60a387fa64ede
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 15:04:00 +0100

    Initialize empty project

---
+++ RPNCalculator-Kata.Tests/RPNCalculatorTest.cs
@@ -0,0 +1,14 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace RPNCalculator_Kata.Tests
+{
+    public class RPNCalculatorTest
+    {
+        [Test]
+        public void test()
+        {
+            Check.That(true).IsTrue();
+        }
+    }
+}
