3af9f0fd0c8e620f2164e0e509fd507d0797c793
Ismaïl <nguyen.ismail@gmail.com>
2017-11-02 15:49:09 +0100

    RPNOperator multiply and divide two number

--- RPNCalculator-Kata.Tests/RPNOperatorTest.cs
+++ RPNCalculator-Kata.Tests/RPNOperatorTest.cs
@@ -34,5 +34,35 @@ namespace RPNCalculator_Kata.Tests
             // THEN
             Check.That(result).IsEqualTo(expectedResult);
         }
+
+        [TestCase(30, 5, 6)]
+        [TestCase(15, 3, 5)]
+
+        public void Sould_Divide_Two_Number(int x, int y, int expectedResult)
+        {
+            // GIVEN
+            Func<int, int, int> divide = RPNOperator.Divide;
+
+            // WHEN
+            int result = divide(x, y);
+
+            // THEN
+            Check.That(result).IsEqualTo(expectedResult);
+        }
+
+        [TestCase(3, 5, 15)]
+        [TestCase(15, 3, 45)]
+
+        public void Sould_Multiply_Two_Number(int x, int y, int expectedResult)
+        {
+            // GIVEN
+            Func<int, int, int> multiply = RPNOperator.Multiply;
+
+            // WHEN
+            int result = multiply(x, y);
+
+            // THEN
+            Check.That(result).IsEqualTo(expectedResult);
+        }
     }
 }
--- RPNCalculator-Kata/RPNOperator.cs
+++ RPNCalculator-Kata/RPNOperator.cs
@@ -11,5 +11,15 @@
         {
             return x - y;
         }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
     }
 }
